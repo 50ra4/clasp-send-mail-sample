810259763a4fc5146f13b0ea853910b32736d054
s.igarashi <50ra4.dev.dev@gmail.com>
2021-02-25 23:56:44 +0900

    fix: replace utils fn to date-fns

--- src/services.ts
+++ src/services.ts
@@ -1,5 +1,11 @@
+import startOfYear from 'date-fns/startOfYear';
+import endOfYear from 'date-fns/endOfYear';
+import differenceInDays from 'date-fns/differenceInDays';
+import format from 'date-fns/format';
+import addDays from 'date-fns/addDays';
+
 import { MESSAGE_TEMPLATE, TO_EMAIL_ADDRESS } from './constants';
-import { getToday, startOfYear, addDays, endOfYear, differenceInDays, formatDate, replaceMessage } from './utils';
+import { getToday, replaceMessage } from './utils';
 
 export const outputHelloWorld = (): void => console.log('hello world!');
 export const onSendMail = (): void => {
@@ -8,7 +14,7 @@ export const onSendMail = (): void => {
   const startOfNextYear = addDays(endOfYear(today), 1);
   const daysPassedThisYear = differenceInDays(today, startOfThisYear);
   const daysLeftThisYear = differenceInDays(startOfNextYear, today);
-  const todayStr = formatDate(today, 'yyyy/MM/dd');
+  const todayStr = format(today, 'yyyy/MM/dd');
   const subject = `【サンプル】今年は残り${daysLeftThisYear}日`;
   const body = replaceMessage(MESSAGE_TEMPLATE, { today: todayStr, daysPassedThisYear, daysLeftThisYear });
   TO_EMAIL_ADDRESS.forEach((to) => MailApp.sendEmail({ to, subject, body }));
--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,4 @@
 export const getToday = (): Date => new Date();
-export const startOfYear = (date: Date): Date => new Date(`${date.getFullYear()}-01-01`);
-export const endOfYear = (date: Date): Date => new Date(`${date.getFullYear()}-12-31`);
-export const addDays = (date: Date, amount: number): Date => {
-  const newDate = new Date(date.toISOString());
-  newDate.setDate(date.getDate() + amount);
-  return newDate;
-};
-export const differenceInDays = (left: Date, right: Date): number =>
-  Math.floor((left.getTime() - right.getTime()) / 86400000);
-export const formatDate = (date: Date, format: string): string => Utilities.formatDate(date, 'JST', format);
 export const replaceMessage = (template: string, replaceParams: Record<string | number, unknown>): string =>
   Object.entries(replaceParams).reduce(
     (acc, [key, value]) => acc.replace(`{${key}}`, typeof value === 'string' ? value : String(value)),
