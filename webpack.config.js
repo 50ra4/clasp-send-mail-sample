fcac30c9b4fa0b13c83b7dff6ee77331b5633a56
s.igarashi <50ra4.dev.dev@gmail.com>
2021-02-25 23:34:56 +0900

    add: webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const GasPlugin = require("gas-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    app: "./src/entryPoint.ts",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "entryPoint.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: "ts-loader",
+      },
+    ],
+  },
+  plugins: [new GasPlugin()],
+  resolve: {
+    extensions: [".ts"],
+  },
+};
