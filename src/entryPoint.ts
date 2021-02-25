fbadc5d5eb8910637c599c6686161a38809f7101
s.igarashi <50ra4.dev.dev@gmail.com>
2021-02-25 23:49:26 +0900

    add: utils.ts

--- src/entryPoint.ts
+++ src/entryPoint.ts
@@ -1,21 +1,5 @@
 import { MESSAGE_TEMPLATE, TO_EMAIL_ADDRESS } from './constants';
-
-// utils
-const getToday = () => new Date();
-const startOfYear = (date: Date) => new Date(`${date.getFullYear()}-01-01`);
-const endOfYear = (date: Date) => new Date(`${date.getFullYear()}-12-31`);
-const addDays = (date: Date, amount: number): Date => {
-  const newDate = new Date(date.toISOString());
-  newDate.setDate(date.getDate() + amount);
-  return newDate;
-};
-const differenceInDays = (left: Date, right: Date) => Math.floor((left.getTime() - right.getTime()) / 86400000);
-const formatDate = (date: Date, format: string): string => Utilities.formatDate(date, 'JST', format);
-const replaceMessage = (template: string, replaceParams: Record<string | number, unknown>): string =>
-  Object.entries(replaceParams).reduce(
-    (acc, [key, value]) => acc.replace(`{${key}}`, typeof value === 'string' ? value : String(value)),
-    template,
-  );
+import { getToday, startOfYear, addDays, endOfYear, differenceInDays, formatDate, replaceMessage } from './utils';
 
 export const outputHelloWorld = (): void => console.log('hello world!');
 export const onSendMail = (): void => {
