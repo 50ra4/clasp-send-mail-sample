d9d63e08b131a0269d3c2f0b30937069bb81fe7c
s.igarashi <50ra4.dev.dev@gmail.com>
2021-03-02 21:51:55 +0900

    fix: scripts/utils

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -4,13 +4,15 @@ export const readJsonFile = <T = unknown>(filename: string): T => JSON.parse(fs.
 export const createJsonFile = <T = unknown>(filename: string, data: T): void =>
   fs.writeFileSync(filename, JSON.stringify(data));
 
-export const tryCatch = <T extends unknown[]>(tryer: (...args: T) => void, catcher?: (err?: any) => void) => (
+export const tryCatch = <T extends unknown[]>(tryer: (...args: T) => void, catcher?: (err: any) => void) => (
   ...args: T
 ): void => {
   try {
     tryer(...args);
   } catch (err) {
-    catcher(err);
+    if (catcher) {
+      catcher(err);
+    }
     console.error(err);
     process.exit(1);
   }
